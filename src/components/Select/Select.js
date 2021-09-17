923533e235ef313d02fabaa8babbd50605cc19c0
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2021-09-17 21:23:37 +0200

    feat: implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,48 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Container>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      {displayedValue}
+      <Icon id="chevron-down" size={24} strokeWidth={2} />
+    </Container>
   );
 };
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  width: fit-content;
+  padding: 12px 16px;
+  gap: 24px;
+  border-radius: 8px;
+
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline-style: solid;
+    outline-style: auto;
+  }
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  opacity: 0;
+  border: none;
+`;
 
 export default Select;
